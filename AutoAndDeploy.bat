70f89c1e6ea6d9dc94b8248c9b761c163d159659
Danil026 <d.gilyazov26@gmail.com>
2025-01-22 12:35:26 +0300

    Автоматизация на Виндоус

---
+++ AutoAndDeploy.bat
@@ -0,0 +1,2 @@
+cd C:\Users\User\PycharmProjects\venvAutodep\venv\Автодепл
+C:\Users\User\PycharmProjects\venvAutodep\venv\Scripts\python.exe C:\Users\User\PycharmProjects\venvAutodep\venv\Автодепл\AutoAndDeploy.py
---
+++ Writedb.bat
@@ -0,0 +1,3 @@
+cd C:\Users\User\PycharmProjects\venvAutodep\venv\Автодепл
+C:\Users\User\PycharmProjects\venvAutodep\venv\Scripts\python.exe C:\Users\User\PycharmProjects\venvAutodep\venv\Автодепл\DB_singleton.py
+C:\Users\User\PycharmProjects\venvAutodep\venv\Scripts\python.exe C:\Users\User\PycharmProjects\venvAutodep\venv\Автодепл\Writedb.py
